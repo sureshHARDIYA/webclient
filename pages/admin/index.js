b0eaae3fb30e185e0aa39cfa2ace609e44ef8f6f
Hung(Jim) <hunghk.it@gmail.com>
2019-01-15 23:39:48 +0700

    simple dashboard

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { idRequired } from 'helpers/isAuth';
 import App from 'components/Admin';
+import Dashboard from 'components/Dashboard';
 
 class Admin extends Component {
   render() {
     return (
       <App {...this.props}>
-        <h1>Dashboard</h1>
+        <Dashboard />
       </App>
     );
   }
